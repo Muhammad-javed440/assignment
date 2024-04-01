28faa3b25a9447d7935f5b246e23cb60ce01c9d5
Muhammad-javed440 <meherjaved440@gmail.com>
2024-04-02 01:29:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+"use strict";
+// Q1: Declare a variable for storing your favorite movie name with a string type annotation.
+let favoriteMovie = 'BAGHBAN';
+console.log(`\n My favourite movie is: ${favoriteMovie} .    \n`);
+// Q2: Create a template string to introduce yourself, including your name and age.
+let firstName = 'Muhammad', lastName = 'Javed', myAge = 43;
+console.log(`\nMy name is ${firstName} ${lastName} and iam ${myAge} years old.\n`);
+// Q3: Initialize a constant with a type annotation for your current mood as a boolean.
+const mood = true;
+console.log(`Today am i happy ? ${mood}.`);
+// Q4: Combine a user's first and last name using template literals and log the full name.
+let firstName1 = 'Muhammad', lastName2 = 'Javed';
+console.log(`\nMy name is ${firstName} ${lastName}.\n`);
+// Q5: Create a variable for a user's age with a type annotation and assign a number to it.
+let userAge = 43;
+console.log(`User age is :${userAge}.`);
+// Q6: Use a template string to display a product name and its price.
+const productName = "Computer";
+const price = 50000;
+const productInfo = `\nI buy a ${productName} and its price is ${price} rupees.\n`;
+console.log(productInfo);
+// Q7: Declare a variable to store the number of pets you have with a number type annotation.
+let numOfPets = 2;
+console.log(`I have ${numOfPets} pets only.`);
+// Q8: Declare a variable with a boolean type annotation indicating if it is your birthday today.
+let isBirthday = false;
+console.log(`\n    Today is my birth day? ${isBirthday} .\n   `);
+// Q9: Create a template string that includes a user's first name, last name, and age with proper labels.
+const userFirstName = "Muhammad";
+const userLastName = "Javed";
+const age = 43;
+const userInfo = `FirstName: ${userFirstName}
+LastName: ${userLastName}
+Age: ${age}`;
+console.log(userInfo);
+// Q10: Initialize a constant to hold your birth year with a number type annotation.
+const birthYear = 1981;
+console.log(`\n    My birth year is : ${birthYear}   \n`);
+// Q11: Write a line of code that uses a template string to combine a city and country into a full address.
+const city = "Lahore";
+const country = "Pakistan";
+const fullAddress = `I live in ${city},${country}.`;
+console.log(fullAddress);
+// Q12: Declare a variable for today's temperature with a number type annotation and assign a value to it.
+let todayTemp = 26;
+console.log(`Today the temperature of lahore is ${todayTemp} degree celsius.`);
